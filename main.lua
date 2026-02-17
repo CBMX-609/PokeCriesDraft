cb61c625e59cb6129e0e925405a6544131f89e80
Emma Vedel <emmavedelpedersen@gmail.com>
2026-02-17 01:01:42 +0100

    simplify file loading

--- main.lua
+++ main.lua
@@ -1,11 +1,4 @@
-local sound_files = NFS.getDirectoryItems(SMODS.current_mod.path .. 'assets/sounds')
-
-for _, path in ipairs(sound_files) do
-  SMODS.Sound {
-    key = path:sub(1, -5),
-    path = path
-  }
-end
+SMODS.Sound:register_global()
 
 function pcry_safely_play_sound(sound, pitch, volume)
   if not SMODS.Sounds[sound] then return end
